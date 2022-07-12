f42f701d93a3fc651bbdd81de898f44747a2809c
Alumno <asd@asd.com>
2022-07-12 18:01:47 +0200

    PRueba

---
+++ app/src/main/java/com/example/proyect_figma/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.proyect_figma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyect_figma.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+    }
+}
